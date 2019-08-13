b67e993d52e4c0bd7085a211754774223c1f91f4
MK <atrvicky@live.com>
2019-08-14 00:33:04 +0530

    Update main.py
    + Update main.py to support eventLoop

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from machine import Pin as pin
 def resetBtnFlash():
     btn_flash = pin(0, pin.OUT)
     btn_flash.off()
+    print("starting EventLoop")
 
 def main():
     resetBtnFlash()
     # do something in the main loop
-    while True:
-        print("banana")
-        utime.sleep(1)
+    import event
